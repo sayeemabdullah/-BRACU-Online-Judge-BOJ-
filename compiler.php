6d2e8b0f3d2467a047cfef31f8ec270740a74225
Sayeem Abdullah <sayeem.mdabdullah@gmail.com>
2019-11-10 02:00:54 +0600

    Beta version 1.9

--- compiler.php
+++ compiler.php
@@ -21,12 +21,10 @@
         <link rel="stylesheet" href="style.css">
         <link href='https://fonts.googleapis.com/css?family=Audiowide' rel='stylesheet'>
         <link href='https://fonts.googleapis.com/css?family=Fredoka One' rel='stylesheet'>
-
 <style>
 textarea{
   font-family: "Courier New";
 }
-
 h1{
   font-family: 'Source Code Pro';
 }
@@ -34,79 +32,42 @@ h4{
   font-family: 'Fredoka One';
 }
 </style>
-
-
-
-
 </head>
 <body>
 <div class="topbar">
   <div class="topnav-right">
         <a href="index.php" float:left>BRACU ONLINE JUDGE (BOJ)</a>
 </div>
-        <a href="compiler.php"><i class="fa fa-terminal"></i> Compiler</a>
-        <a href="problemset.php"><i class="fa fa-tasks"></i> Problem Set</a>
+        <a href="compiler.php"><i class="fa fa-terminal"></i> Online Compiler</a>
+        <a href="problemset.php"><i class="fa fa-puzzle-piece"></i> Problem Archive</a>
+        <a href="submissionstat.php"><i class="fa fa-user"></i> My Account</a>
 </div>
-
-
-<div class="row log">
-<div class="col-sm-10">
-<div class=""><h1 style="text-align:center;"><b>Online Compiler<b></h1></div>
-</div>
-
-<div class="col-sm-1">
-
-</div>
-
-<div class="col-sm-1">
-  
-</div>
-
-</div>
-
-
-
-
 <div class="row cspace">
 <div class="col-sm-8">
 <div class="form-group">
 <form action="compile.php" id="form" name="f2" method="POST" >
 <label for="lang"><h4>Language :</h4></label>
-
 <select class="form-control" name="language">
 <option value="c">C</option>
 <option value="cpp">C++</option>
 <option value="cpp11">C++11</option>
 <option value="java">Java</option>
-
-
 </select><br><br>
-
 <label for="ta"><h4>SOURCE CODE :</h4></label>
 <textarea class="form-control" name="code" rows="10" cols="50"></textarea><br><br>
 <label for="in"><h4>Enter Sample Input :</h4></label>
 <textarea class="form-control" name="input" rows="10" cols="50"></textarea><br><br>
 <input type="submit" id="st" class="btn btn-success btn-lg btn-block" value="Run Code"><br><br><br>
-
-
 </form>
-
 <script type="text/javascript">
   
   $(document).ready(function(){
-
      $("#st").click(function(){
   
            $("#div").html("Finger Crossed...loading");
-
-
      });
-
   });
-
-
 </script>
-
 <script>
 //wait for page load to initialize script
 $(document).ready(function(){
@@ -114,7 +75,6 @@ $(document).ready(function(){
     $('form').on('submit', function(e){
       //prevent form from submitting and leaving page
       e.preventDefault();
-
       // AJAX 
       $.ajax({
             type: "POST", //type of submit
@@ -123,7 +83,6 @@ $(document).ready(function(){
             datatype: "html", //expected data format from process.php
             data: $('form').serialize(), //target your form's data and serialize for a POST
             success: function(result) { // data is the var which holds the output of your process.php
-
                 // locate the div with #result and fill it with returned data from process.php
                 $('#div').html(result);
             }
@@ -131,7 +90,6 @@ $(document).ready(function(){
     });
 });
 </script>
-
 <label for="out"><h4>Output :</h4></label>
 <textarea id='div' class="form-control" name="output" rows="10" cols="50"></textarea><br><br>
 <a href="compiler.php" class="btn btn-danger btn-lg btn-block" role="button">Refresh</a>
@@ -149,15 +107,9 @@ function submitForm(oFormElement)
 </script>-->
 <!--<label for="out">Output</label>
 <textarea id='div' class="form-control" name="output" rows="10" cols="50"></textarea><br><br>-->
-
-
-
-
 </div>
 </div>
-
 <div class="col-sm-4">
-
   
 </div>
 </div>
@@ -168,10 +120,7 @@ function submitForm(oFormElement)
 <div class="row area">
 <div class="col-sm-3">
 </div>
-
 <div class="col-sm-5">
-
-
 <div class="fm">
 BETA VERSION<bR>
 © 2019 BRACU Online Judge (BOJ)<br>
@@ -182,5 +131,3 @@ BETA VERSION<bR>
 </div>
 </body>
 </html>
-
-
